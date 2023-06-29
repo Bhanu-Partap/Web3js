ade588a49a1ee34a51ea26a471c1a87414b8757d
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-06-29 12:34:37 +0530

    update

--- calling_methods.js
+++ calling_methods.js
@@ -1,9 +1,7 @@
 const { Web3 } = require('web3');
-const provider = "http://127.0.0.1:7545";
-const web3 = new Web3(provider);
+const web3 = new Web3("http://127.0.0.1:7545");
 
-
-web3.eth.Contract([
+const abi =[
 	{
 		"inputs": [
 			{
@@ -43,53 +41,22 @@ web3.eth.Contract([
 		"stateMutability": "view",
 		"type": "function"
 	}
-],"0x20775d300BdE943Ac260995E977fb915fB01f399");
+]
+const address="0x9aD5CF2148fF735069405D8717c5A54F4E1bD4c5"
 
+const contract =  new web3.eth.Contract(abi,address);
+// contract.methods.x().call().then(console.log);
 
-Contract.methods.x().call().then(console.log);
+//=====================changing the value of x in contract
 
+//**********=====+++++thers a bug that if we didnt use await then after rewriting the value of x it still shows the old value.**********=====+++++//
 
+const call =(async()=>{
+await contract.methods.set(25).send({from :"0x50e87F67a6049e94EEF38B5e09Bed9E937cc2E47"});
 
+await contract.methods.x().call().then(console.log);
+})
 
-// const contract =   web3.eth.Contract([
-// 	{
-// 		"inputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "_x",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"name": "set",
-// 		"outputs": [],
-// 		"stateMutability": "nonpayable",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "contractAddress",
-// 		"outputs": [
-// 			{
-// 				"internalType": "address",
-// 				"name": "",
-// 				"type": "address"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	},
-// 	{
-// 		"inputs": [],
-// 		"name": "x",
-// 		"outputs": [
-// 			{
-// 				"internalType": "uint256",
-// 				"name": "",
-// 				"type": "uint256"
-// 			}
-// 		],
-// 		"stateMutability": "view",
-// 		"type": "function"
-// 	}
-// ],"0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B");
+call();
+
 
