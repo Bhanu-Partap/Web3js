bfcd5984f86a79a37fc733796be0dba444b76f9c
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-06-29 11:34:36 +0530

    update

--- calling_methods.js
+++ calling_methods.js
@@ -1,8 +1,9 @@
-const {Web3} = require("web3");
-const provider = "HTTP://127.0.0.1:7545";
+const { Web3 } = require('web3');
+const provider = "http://127.0.0.1:7545";
 const web3 = new Web3(provider);
 
-const ABI = [
+
+web3.eth.Contract([
 	{
 		"inputs": [
 			{
@@ -42,8 +43,53 @@ const ABI = [
 		"stateMutability": "view",
 		"type": "function"
 	}
-]
+],"0x20775d300BdE943Ac260995E977fb915fB01f399");
 
-const contract =  web3.eth.Contract(ABI, "0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B");
 
-contract.methods.set().call().then(console.log);
+Contract.methods.x().call().then(console.log);
+
+
+
+
+// const contract =   web3.eth.Contract([
+// 	{
+// 		"inputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "_x",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"name": "set",
+// 		"outputs": [],
+// 		"stateMutability": "nonpayable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "contractAddress",
+// 		"outputs": [
+// 			{
+// 				"internalType": "address",
+// 				"name": "",
+// 				"type": "address"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"inputs": [],
+// 		"name": "x",
+// 		"outputs": [
+// 			{
+// 				"internalType": "uint256",
+// 				"name": "",
+// 				"type": "uint256"
+// 			}
+// 		],
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	}
+// ],"0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B");
+
